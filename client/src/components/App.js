4516f615a6ee7c16789e8f491f1ac59b57b49c3c
Asad <asadaliazam1@gmail.com>
2018-06-26 14:07:31 -0700

    merged some code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,13 +17,10 @@ import Chart from './chart';
 import Register from './form';
 import ProfileCompletion from './profileCompletion';
 import SchoolProfile from './schoolProfile';
-<<<<<<< HEAD
 import CostLiving from './cost_living';
 import FieldStudy from './field_study';
 import Selector from './selector';
-=======
 import Snowfall from './snowfall';
->>>>>>> e7910c2b384ff58d1c2d108033302a455bfb8805
 
 class App extends Component {
   render() {
@@ -43,13 +40,10 @@ class App extends Component {
         <li><Link to="/profile">Profile</Link></li>
         <li><Link to="/profileCompletion">ProfileCompletion</Link></li>
         <li><Link to="/schoolProfile">School Profile</Link></li>
-<<<<<<< HEAD
         <li><Link to="/cost_living">Cost Living</Link></li>
         <li><Link to="/field_study">Field Study</Link></li>
         <li><Link to="/selector">Selector</Link></li>
-=======
         <li><Link to="/snowfall">Snowfall</Link></li>
->>>>>>> e7910c2b384ff58d1c2d108033302a455bfb8805
       </ul>
         <hr/>
         <Route path="/chart" component={Chart} />
@@ -59,12 +53,9 @@ class App extends Component {
         <Route path="/profile" component={Profile} />
         <Route path="/profileCompletion" component={ProfileCompletion} />
         <Route path="/schoolProfile" component={SchoolProfile} />
-<<<<<<< HEAD
         <Route path="/cost_living" component={CostLiving} />
         <Route path="/selector" component={Selector} />
-=======
         <Route path="/snowfall" component={Snowfall} />
->>>>>>> e7910c2b384ff58d1c2d108033302a455bfb8805
     </div>
     </Router>
       </div>
