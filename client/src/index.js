331c820783487ec01600893b4b7b19cdb8bd7ce1
Samaneh <advisors.samantha@gmail.com>
2018-07-24 18:12:19 -0700

    Merging all css files to one style.css file/design homepage:24 July

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './rod.css';
-import './sam.css';
+
+import './styles.css';
 import App from './components/App';
 
 ReactDOM.render(
