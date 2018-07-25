432ff45cd3f285e4ad5a662b8c742a2880fe8401
lakshaysachdeva <35813293+lakshaysachdeva@users.noreply.github.com>
2018-07-24 20:30:27 -0700

    rod: sync of LandingPage and CSS for Lakshay

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Switch} from 'react-router';
+
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+import Logo from'./img/logo1.svg';
+
+import Landingimage from'./img/landing-header.jpg';
+import Button from '@material-ui/core/Button';
+import CostOfLivingGraph from './costOfLivingGraph';
+
+
+
+
+
 
 class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    }
 
   render() {
     return (
-          <div>
+          <div className="landingPage">
+          <div className="landing-header">
+          <div className="landing-logo">
+          <img src={Logo} alt="Eduvisors logo"/>
+          </div>
             <ul>
               <li>
                 <Link to="/login">Login</Link>
@@ -13,7 +34,28 @@ class LandingPage extends Component {
               <li>
                 <Link to="/register">Register</Link>
               </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+
             </ul>
+            <div className="signupButton">
+           <p>Sign Up</p>
+           <Button  type="submit" variant="contained" color="primary" className="signupButton">
+             <Link to="/HomePage/signup">Sign up</Link>
+           </Button>
+         </div>
+            </div>
+            <div className="landing-image">
+            <img src={Landingimage} alt="Landingimage header-image"/>
+            </div>
+            <div>
+                     <CostOfLivingGraph province='BC' />
+                   </div>
+
           </div>
     );
   }
