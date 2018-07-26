1bfd5138d499db46d12cc95ea0816b6420b56c45
Samaneh <advisors.samantha@gmail.com>
2018-07-26 15:08:17 -0700

    26 July homepage design

--- client/src/components/HomeContent.js
+++ client/src/components/HomeContent.js
@@ -10,16 +10,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUniversity } from '@fortawesome/free-solid-svg-icons'
 import { faUsers } from '@fortawesome/free-solid-svg-icons'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-// ===============Material UI design for the graphs==========
-//
-// import PropTypes from 'prop-types';
-// import { withStyles } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
-// import { fagraduationCap } from '@fortawesome/free-solid-svg-icons'
+
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class HomeContent extends Component {
   constructor(props) {
@@ -34,102 +33,122 @@ class HomeContent extends Component {
 
 
   render() {
+    const classes = this.props;
     return (
-      <div className="App">
-          <div className="main-content">
-            <form onSubmit={this.handleSubmit}>
-              <label>
-
-                Choose a Province:
-                <select value={this.state.value} onChange={this.handleChange}>
-                  <option value="AB" label="Alberta">AB</option>
-                  <option value="BC" label="British Columbia">BC </option>
-                  <option value="NL" label="NewFoundLand">NL</option>
-                  <option value="MB" label="Manitoba">MB</option>
-                  <option value="ON" label="Ontario">ON</option>
-                  <option value="PE" label="Prince Ed Island">PE</option>
-                  <option value="NS" label="Nova Scotia">NS</option>
-                  <option value="NB" label="New Brunswick">NB</option>
-                  <option value="QC" label="Quebec">QC</option>
-                  <option value="SK" label="Saskatchewan">SK</option>
-                </select>
-              </label>
-              <div className="information">
-                <ul>
-                  <li><FontAwesomeIcon icon={faUniversity} />100</li>
-                  <li><FontAwesomeIcon icon={faUsers} />9.000</li>
-                  <li><FontAwesomeIcon icon={faCoffee} />Aberdeen</li>
-                  {/* <li><FontAwesomeIcon icon={fagraduationCap} />111</li> */}
-              </ul>
-              </div>
-
-            </form>
 
 
 
+          <div className="Content">
+            <div className="surveyLink">
+                <p>Take the survey and we will find your matchmake university:</p>
+                <Button  type="submit" variant="contained" color="primary" className="surveyButton">
+                    <Link to="/HomePage/survey">Take Survey</Link>
+                </Button>
+            </div>
 
-            {/* <Card >
-        <CardContent>
-          <Typography color="textSecondary">
-            Word of the Day
-          </Typography>
-          <Typography variant="headline" component="h2">
-            benevolent
-          </Typography>
-          <Typography  color="textSecondary">
-            adjective
-          </Typography>
-          <Typography component="p">
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-           <div className = "charts"> */}
-                <div className="top-chart">
-                {/* <p> {this.state.value} </p> */}
+
+            <div className="top-content">
+              <form onSubmit={this.handleSubmit}>
+                  <label>
+                    Choose a Province:
+                    <select value={this.state.value} onChange={this.handleChange} >
+                      <option value="AB" label="Alberta">AB</option>
+                      <option value="BC" label="British Columbia">BC </option>
+                      <option value="NL" label="NewFoundLand">NL</option>
+                      <option value="MB" label="Manitoba">MB</option>
+                      <option value="ON" label="Ontario">ON</option>
+                      <option value="PE" label="Prince Ed Island">PE</option>
+                      <option value="NS" label="Nova Scotia">NS</option>
+                      <option value="NB" label="New Brunswick">NB</option>
+                      <option value="QC" label="Quebec">QC</option>
+                      <option value="SK" label="Saskatchewan">SK</option>
+                    </select>
+                  </label>
+                  <div className="information">
+                    <ul>
+                      <li><FontAwesomeIcon icon={faUniversity} />100</li>
+                      <li><FontAwesomeIcon icon={faUsers} />9.000</li>
+                      <li><FontAwesomeIcon icon={faCoffee} />Aberdeen</li>
+
+                    </ul>
+                  </div>
+                </form>
+
+              </div>   {/* end of TOP-CONTENT */}
+
+                <div className="top-chart charts">
                   <Snowfall province={this.state.value} type={'snow'} />
                 </div>
-                {/* <div>
-                  <Snowfall province={this.state.value} type={'rain'} />
-                </div>
-                <div>
-                  <Snowfall province={this.state.value} type={'temp_low'} />
-                </div>
-                <div>
-                  <Snowfall province={this.state.value} type={'temp_high'} />
-                </div>
-                <div>
-                  <Snowfall province={this.state.value} type={'temp_avg'} />
-                </div> */}
-                <div className="down-charts">
-                    <div>
-                      <EmploymentGraph province={this.state.value} />
-                    </div>
-                    <div>
-                      <CostOfLivingGraph province={this.state.value} />
-                    </div>
-                    <div>
-                      <TuitionGraph province={this.state.value} />
-                    </div>
-                    <div>
-                      <SchoolRankGraphList province={this.state.value} />
-                    </div>
+
+
+                <div className="down-charts charts">
+                  <Card className="resultPageCard">
+
+                        <CardContent className="CardContent">
+                            {/* <Typography component="h2">
+                              {schoolNames.institutionName}
+                            </Typography> */}
+                            <EmploymentGraph province={this.state.value} />
+                            <Typography component="p">
+                              Weather Graph: {this.state.value}
+                            </Typography>
+
+                        </CardContent>
+                  </Card>
+
+                  <Card className="resultPageCard">
+
+                        <CardContent className="CardContent">
+                            {/* <Typography component="h2">
+                              {schoolNames.institutionName}
+                            </Typography> */}
+                            <CostOfLivingGraph province={this.state.value} />
+                            <Typography component="p">
+                              Cost Of Living: {this.state.value}
+                            </Typography>
+
+                        </CardContent>
+                  </Card>
+
+                  <Card className="resultPageCard">
+
+                        <CardContent className="CardContent">
+                            {/* <Typography component="h2">
+                              {schoolNames.institutionName}
+                            </Typography> */}
+                            <TuitionGraph province={this.state.value} />
+                            <Typography component="p">
+                              Tuition Fees Graph: {this.state.value}
+                            </Typography>
+
+                        </CardContent>
+                  </Card>
+
+                  <Card className="resultPageCard">
+
+                        <CardContent className="CardContent">
+                            {/* <Typography component="h2">
+                              {schoolNames.institutionName}
+                            </Typography> */}
+                            <SchoolRankGraphList province={this.state.value} />
+                            <Typography component="p">
+                              School Ranking Graph: {this.state.value}
+                            </Typography>
+
+                        </CardContent>
+                  </Card>
 
 
 
+                </div> {/* end of DOWN-CHARTS */}
 
-              </div>
-            {/* </div> */}
           </div>
-        </div>
+
     );
   }
-
 }
 
+// export default withStyles(styles)(HomeContent);
 export default HomeContent;
+
 // export default with()(SimpleCard);
