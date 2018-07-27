dfea75b74c5012e7fcc34f8269fde93ff83426a2
lakshaysachdeva <35813293+lakshaysachdeva@users.noreply.github.com>
2018-07-26 20:48:23 -0700

    lakshay: landing page css

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import Logo from'./img/logo1.svg';
-
+import Menu from './Menutwo.js';
 import Landingimage from'./img/landing-header.jpg';
 import Button from '@material-ui/core/Button';
 import CostOfLivingGraph from './costOfLivingGraph';
-
+import SnowFall from './snowfall';
+import Feature1 from'./img/like.svg';
+import Feature2 from'./img/lock.svg';
+import Feature3 from'./img/map.svg';
 import Footer from './Footer.js';
 
 
@@ -14,27 +16,11 @@ class LandingPage extends Component {
 
   render() {
     return (
+
           <div className="landingPage">
+
           <div className="landing-header">
-          <div className="landing-logo">
-          <img src={Logo} alt="Eduvisors logo"/>
-          </div>
-            <ul>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-
-            </ul>
-
+        <Menu toggleProf={this.toggleProf}/>
             </div>
 
             <div className="landing-image">
@@ -58,18 +44,29 @@ class LandingPage extends Component {
       </div>
       </div>
             <div className="landing-chart1">
-                     <CostOfLivingGraph province='BC' />
+              <SnowFall province='BC' type='rain' />
                    </div>
                    <div className="landing-features">
-                   <div className="feature-1 features"><span>Feature 1</span><h3>About feature</h3></div>
-                   <div className="feature-2 features"><span>Feature 2</span><h3>About feature</h3></div>
-                   <div className="feature-3 features"><span>Feature 3</span><h3>About feature</h3></div>
+                   <div className="feature-1 features"><img src={Feature1} alt="Feature1"/><h3>Eduvisors Engine</h3></div>
+                   <div className="feature-2 features"><img src={Feature2} alt="Feature2"/><h3>Secured Personal Data</h3></div>
+                   <div className="feature-3 features"><img src={Feature3} alt="Feature3"/><h3>Province & School Specific Details</h3></div>
                    <div className="landing-chart2">
-                            <CostOfLivingGraph province='BC' />
+                     <CostOfLivingGraph province='BC' />
+
                           </div>
 
                    </div>
-<Footer />          </div>
+                   {/* <div className="team">
+                   <ul>
+                   <li><img src={Feature2} alt="Feature2"/><h3>Secured Personal Data</h3></li>
+                   <li></li>
+                   <li></li>
+                   <li></li>
+                   <li></li>
+                   </ul>
+                   </div> */}
+<Footer />
+</div>
     );
   }
 }
