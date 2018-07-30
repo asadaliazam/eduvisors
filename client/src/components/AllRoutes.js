635ace729968fcc3db23877fc6b3061a1c12139b
Rodrigo-Ornellas <rodrigo.ornellas@mail.com>
2018-07-30 11:16:58 -0700

    rod: Mobile MENUS NOT working. Gave up on the Responsive Drawer from Material UI - file: HomePage2.js

--- client/src/components/AllRoutes.js
+++ client/src/components/AllRoutes.js
@@ -4,21 +4,18 @@ import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 import LandingPage from './LandingPage';
 import HomePage from './HomePage';
-
-
+// import ResponsiveDrawer from './HomePage2'
 
 class AllRoutes extends Component {
 
   render() {
     return (
-
                 <Switch>
                       <Route exact path="/" component={LandingPage} />
                       <Route path="/login" component={LoginForm} />
                       <Route exact path="/register" component={RegisterForm} />
                       <Route path="/HomePage" component={HomePage} />
                 </Switch>
-      
     );
   }
 }
