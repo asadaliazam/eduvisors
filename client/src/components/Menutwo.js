cf6b7e1bcb1213f6f11997d7327db2d029075935
LAPTOP-873C8UPI\Monty <navrosesharma@gmail.com>
2018-07-31 16:16:31 -0700

    Testing Landing Page

--- client/src/components/Menutwo.js
+++ client/src/components/Menutwo.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 // import { faUser } from '@fortawesome/free-solid-svg-icons'
+import Media from "react-media";
 
 
   class LongMenu extends React.Component {
@@ -13,8 +14,19 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
       super (props);
       this.state = {
         anchorEl: null,
+          shown: false,
       };
     }
+
+
+
+
+            toggle() {
+            		this.setState({
+            			shown: !this.state.shown
+            		});
+            	}
+
 
     handleClick = event => {
       this.setState({ anchorEl: event.currentTarget });
@@ -27,6 +39,18 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
   render() {
     // const { anchorEl } = this.state;
+
+
+
+
+                var shown = {
+                			display: this.state.shown ? "block" : "none"
+                		};
+
+                		var hidden = {
+                			display: this.state.shown ? "none" : "block"
+                		}
+
     return (
 
         <header>
@@ -46,9 +70,29 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
                         {/* <li><Link to="/register">Signup</Link></li> */}
                     </ul>
                 </nav>
-                <div className="mobile" id="bars">
-                      <FontAwesomeIcon icon={faBars} />
-                </div>
+                <Media query="(max-width: 998px)">
+                  <div>
+           <div className="mobile" id="bars" style={ shown }>
+
+
+                 <ul>
+                     <li><a href="/HomePage/about">About</a></li>
+                     <li><a href="/HomePage/contactus">Contact Us</a></li>
+                     <li><Link to="/Login">Login</Link></li>
+                  
+                 </ul>
+
+
+             <h2> xksjadhxjhsbx </h2>
+
+           </div>
+
+           <FontAwesomeIcon icon={faBars} onClick={this.toggle.bind(this)} />
+
+                  </div>
+
+              </Media>
+
               </div>
 
         </header>
