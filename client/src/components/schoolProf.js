f7c9901095613d87f7dd25c142ba0a9b39f186fe
Rodrigo-Ornellas <rodrigo.ornellas@mail.com>
2018-08-01 19:18:04 -0700

    rod: conflict resolution

--- client/src/components/schoolProf.js
+++ client/src/components/schoolProf.js
@@ -6,12 +6,8 @@ import Card from '@material-ui/core/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faTrophy, faMedal } from '@fortawesome/free-solid-svg-icons';
 import CostOfLivingGraph from './costOfLivingGraph';
-<<<<<<< HEAD
 import BackButton from './BackButton.js'
-
-=======
 import EmploymentGraph from './employmentGraph';
->>>>>>> c1e28d0bb253707675be219db4ca4e5514e6eac7
 
 class SchoolProf extends Component {
   constructor(props) {
